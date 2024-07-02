c0099d047a0a446079a1c123d136303c8830e429
Hilmi AM <imiia75775@proton.me>
2024-07-02 12:53:03 +0700

    entity manager factory

---
+++ src/test/java/franxx/code/jpa/EntityManagerTest.java
@@ -0,0 +1,24 @@
+package franxx.code.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EntityManagerTest {
+
+  @Autowired
+  private EntityManagerFactory entityManagerFactory;
+
+  @Test
+  void testEntityManagerFactory() {
+    Assertions.assertNotNull(entityManagerFactory);
+
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    Assertions.assertNotNull(entityManager);
+    entityManager.close();
+  }
+}
